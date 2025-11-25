424c0712dbbbdea3d8b77a7d7a4f4613b414c3ff
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-11-26 01:27:43 +0530

    Updated ClientController.js

--- Backend/Controller/ClientController.js
+++ Backend/Controller/ClientController.js
@@ -1,4 +1,10 @@
-const Model=require("../Model/ClintModel")
+const Model = require("../Model/ClintModel")
+const Admin = require("firebase-admin")
+const serviceAccount = require("./loan-management-platform-750c5-firebase-adminsdk-fbsvc-35b48e95ad.json");
+Admin.initializeApp({
+  credential: Admin.credential.cert(serviceAccount),
+ projectId: "loan-management-platform-750c5", // ✅ your Project ID
+});
 exports.LoanApplyVerification  =async (req, res, next) => {
     const LoanData = req.body; 
     try {
@@ -23,3 +29,52 @@ exports.LoanApplyVerification  =async (req, res, next) => {
         });
     }
 };
+exports.FindBorrower = async (req, res, next) => {
+    const MobileNo = req.body.mobileNo;
+    
+    try {
+        const result = await Model.findBorrowerByPhone(MobileNo)
+        if (result) {
+            res.status(200).json({ exist: true })
+        } else {
+            res.json({ exist: false })
+        }
+        
+    } catch (err) {
+        res.status(500).json({ message: "Server error! try again later" });
+    }
+    
+};
+exports.loanDataForAuthUser =async (req,res,next) => {
+    
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({ message: "Missing authorization token" });
+        }
+
+        const tokenId = authHeader.split(" ")[1];
+        if (!tokenId) {
+            return res.status(401).json({ message: "Invalid authorization header" });
+        }
+        const decoded = await Admin.auth().verifyIdToken(tokenId);
+      console.log( decoded)
+        const result = await Model.loanDataForAuthUser(decoded); 
+        res.status(200).json({user:result})
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+exports.checkToken=async(req,res,next) => {
+      const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    await Admin.auth().verifyIdToken(token);
+    return res.sendStatus(200); // valid
+  } catch (err) {
+    return res.sendStatus(401); // expired or invalid
+  }
+}
