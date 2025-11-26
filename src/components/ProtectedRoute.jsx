bbc9e940c1c7052b3eeacbe2130bdda959307994
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-11-26 22:54:13 +0530

    Updated ProtectedRoute.js

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,21 +2,58 @@ import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/Firebase/firebaseConfig";
+
 const ProtectedRoute = ({ children }) => {
-    const [Login, setLogin] = useState(false)
-    const [Loading, setLoding] = useState(true)
+    const [Login, setLogin] = useState(null);
+    const [validToken, setValidToken] = useState(null)
+
+
     useEffect(() => {
-        const unsuscribe = onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            console.log(user)
             if (user) {
                 setLogin(true)
-                setLoding(false)
+                const token = localStorage.getItem("token")
+                console.log("Token sent to backend:", token);
+                const res = await fetch("http://localhost:3000/check-token", {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (res.status === 200) {
+                    setValidToken(true)
+                }
+                else {
+                    setValidToken(false)
+                    auth.signOut();
+                }
             } else {
-                setLoding(false)
+                setLogin(false)
+                setValidToken(false)
             }
-        })
-        return () => unsuscribe()
-    }, [])
-    if (Loading) return <p>Loading.....</p>
-    return Login ? children : <Navigate to="/" replace />;
-}
+
+
+        });
+        return () => unsubscribe();
+    }, []);
+    console.log(Login, validToken)
+
+
+
+
+    var authorized = Login === true && validToken === true;
+    console.log(authorized)
+    if (Login === null || validToken === null) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <p className="text-3xl font-bold">Loading...</p>
+            </div>
+        );
+    }
+
+    return authorized ? children : <Navigate to="/" replace />;
+
+};
+
 export default ProtectedRoute;
