723b838e26418b6cc89490746829eebd384140b4
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-11-28 13:31:05 +0530

    Updted store.js

--- src/Store/store.js
+++ src/Store/store.js
@@ -3,7 +3,7 @@ const useStore = create((set) => ({
     
         TotalLoanAmount: 0,
             TotalEMIAmount: 0,
-                TotalActiveBorrower: 0,
+                TotalActiveBorrowers: 0,
                     EMIPending: 0,
                         ApplicationDue: 0
     
