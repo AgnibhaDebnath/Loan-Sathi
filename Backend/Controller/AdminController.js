a2a32a41eb791bf5aa1bb7e743e14c864c231a4f
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-11-29 18:13:04 +0530

    Updated AdminController.js

--- Backend/Controller/AdminController.js
+++ Backend/Controller/AdminController.js
@@ -12,5 +12,16 @@ LoanDetails
         }
     
     
-    }
+}
+exports.Update_statsu_of_borrower = async(req, res, next) => {
+    const { number, status } = req.body;
+    console.log(number, status);
+    try {
+        const result = await AdminModel.Update_statsu_of_borrower_on_database(number, status)
+     
+        res.status(200).json({"successMessage":result.message})
+    } catch (err) {
+        console.log(err)
+}
+}
 
