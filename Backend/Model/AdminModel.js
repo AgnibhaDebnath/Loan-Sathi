2169ecc4e9654568b4161b60c4a1f573f49a351b
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-10 11:21:01 +0530

    Updated AdminModel.js

--- Backend/Model/AdminModel.js
+++ Backend/Model/AdminModel.js
@@ -3,7 +3,7 @@ const connection=require("../DB/db")
 const LoanDetails = async () => {
     const sql = "SELECT * FROM loan_application_data" 
     try {
-        const [rows] = await connection.query(sql);
+        const [rows] = await connection.execute(sql);
         return rows;
 
     } catch (error) {
@@ -17,7 +17,7 @@ const Update_statsu_of_borrower_on_database = async (number, status) => {
     try {
         const sql = "UPDATE loan_application_data SET loan_status = ?, decision_at=CURRENT_TIMESTAMP WHERE mobile_no=?"
     
-        const [rows] = await connection.query(sql, [status, number])
+        const [rows] = await connection.execute(sql, [status, number])
         if (rows.affectedRows > 0) {
             return { "success": true, "message": "Loan status update successfull" };
         }
@@ -26,8 +26,28 @@ const Update_statsu_of_borrower_on_database = async (number, status) => {
         console.log(err)
     }
 }
+const verify_password = async (trimedName) => {
+    try {
+        const [rows] = await connection.execute("SELECT * FROM admin_details WHERE admin_name = ? ", [trimedName]) 
+        if (rows.length == 0) {
+            return null
+        } else {
+        const [{ admin_password }] = rows;
+       
+        return admin_password  
+        }
+
+
+    } catch(err) {
+        console.log(err)
+    }
+   
+
+   
+}
+ 
     
 
     
 
-module.exports = { LoanDetails,Update_statsu_of_borrower_on_database }
+module.exports = { LoanDetails,Update_statsu_of_borrower_on_database,verify_password }
