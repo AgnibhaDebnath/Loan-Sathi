145454942368ca39752ac1b410825441826a3274
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-10 11:23:32 +0530

    Updated AdminRouter.js

--- Backend/Router/AdminRouter.js
+++ Backend/Router/AdminRouter.js
@@ -2,5 +2,6 @@ const express = require("express")
 const AdminRouter = express.Router();
 const Contorller=require("../Controller/AdminController")
 AdminRouter.get("/loan-application", Contorller.getLoanDetails)
-AdminRouter.post("/update-status",Contorller.Update_statsu_of_borrower)
+AdminRouter.post("/update-status", Contorller.Update_statsu_of_borrower)
+AdminRouter.post("/verify-password",Contorller.verify_password)
 module.exports = AdminRouter
