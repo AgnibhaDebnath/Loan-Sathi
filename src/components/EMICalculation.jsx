9abba1e2b035e2ee36922bc9dc2bea147fd3995a
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-15 03:34:59 +0530

    Updated EMIcalculater.jsx

--- src/components/EMICalculation.jsx
+++ src/components/EMICalculation.jsx
@@ -6,11 +6,11 @@ const FloatingEMICalculator = () => {
     const [rate, setRate] = useState("");
     const [months, setMonths] = useState("");
     const [emi, setEmi] = useState(null);
-    const [selectedOption, setSelectedOption] = useState("")
+
 
     const calculateEMI = (e) => {
         e.preventDefault();
-        if (!amount || !selectedOption || !months || !(months > 0)) return;
+        if (!amount || !months || !(months > 0)) return;
 
         const principal = parseFloat(amount);
         const monthlyRate = parseFloat(rate) / 12 / 100;
@@ -34,7 +34,7 @@ const FloatingEMICalculator = () => {
                         setRate("");
                         setMonths("");
                         setEmi("");
-                        setSelectedOption("")
+
                     }
                     setIsOpen(!isOpen)
                 }
@@ -48,7 +48,7 @@ const FloatingEMICalculator = () => {
             {isOpen && (
                 <div className="fixed bottom-20 right-6 bg-white p-5 rounded-2xl shadow-2xl w-80 z-50 border border-gray-200">
                     <div className="flex justify-center">
-                        <h3 className="w-full  text-lg font-medium bg-gradient-to-r from-emerald-800 to-green-500 bg-clip-text text-transparent 
+                        <h3 className="w-full  text-lg font-bold bg-gradient-to-r from-emerald-800 to-green-500 bg-clip-text text-transparent 
                  tracking-wide inline-block mb-3 text-center">
                             EMI Calculator
                         </h3>
@@ -64,33 +64,12 @@ const FloatingEMICalculator = () => {
                             }}
                             className="border w-full p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400 text-base font-medium"
                         />
-                        <p className=" w-full text-gray-700 font-medium text-left">
-                            Choose a loan type:
-                        </p>
-                        <select value={selectedOption} onChange={(e) => {
-                            const value = e.target.value;
-
-                            setSelectedOption(value);
-                            if (value == "Personal") {
-                                setRate(16)
-                            }
-                            else if (value == "Business") {
-                                setRate(12)
-                            }
-                            else {
-                                setRate("")
-                            }
-                        }} className="border p-2 font-medium rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-green-400" >
-                            <option value="">Select a option</option>
-                            <option value="Personal">Personal Loan</option>
-                            <option value="Business">Business Loan</option>
-                        </select>
                         <input
                             type="number"
                             placeholder="Interest Rate (%)"
                             value={rate}
-                            readOnly
-                            className="border p-2 w-full rounded-lg focus:outline-none   text-base font-medium hover:cursor-not-allowed"
+                            onChange={e => setRate(e.target.value)}
+                            className="border p-2 w-full rounded-lg focus:outline-none   text-base font-medium "
                         />
                         <input
                             type="number"
