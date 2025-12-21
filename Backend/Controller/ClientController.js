946601688e4048220d00048a3da81e1e9e9c666a
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-21 21:34:27 +0530

    Updated ClientController.js

--- Backend/Controller/ClientController.js
+++ Backend/Controller/ClientController.js
@@ -9,18 +9,13 @@ exports.LoanApplyVerification  =async (req, res, next) => {
     const LoanData = req.body; 
     try {
         const result = await Model.LoanFormSubmission(LoanData); // ✅ await added
-        if (!result.inserted) {
-            res.status(409).json({
-     message:"Mobile no already exists"
-            })
-        }
-        else {
+        
             res.status(200).json(
                 {
                     message:"Loan application successfull"
                 }
             )
-        }  
+      
     } catch (err) {
         console.error("❌ Loan submission failed:", err.message);
         res.status(500).json({
@@ -81,3 +76,4 @@ exports.checkToken=async(req,res,next) => {
     return res.status(401).json({ message: "Token expired or invalid" }); // expired or invalid
   }
 }
+
