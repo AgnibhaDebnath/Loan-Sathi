728790f23e2f2692b4c92a89170f27698772a0bf
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-21 21:32:24 +0530

    Updated ClientRouter.js

--- Backend/Router/ClientRouter.js
+++ Backend/Router/ClientRouter.js
@@ -1,5 +1,7 @@
 const express = require("express")
+
 const clintRouter = express.Router();
+
 const ClintController=require("../Controller/ClientController")
 clintRouter.post("/apply", ClintController.LoanApplyVerification)
 clintRouter.post("/borrowerLogin", ClintController.FindBorrower)
