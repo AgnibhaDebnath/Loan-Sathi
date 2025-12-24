be230302e1e57125222bd215c4e9706b54399c02
Agnibha Debnath <agnibhadebnath@gmail.com>
2025-12-24 19:36:32 +0530

    Updated AdminLogin.jsx

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -3,19 +3,110 @@ import { useContext, useState } from "react";
 import { Input } from "./ui/input";
 import { X, CircleX, CircleCheck } from "lucide-react";
 import { Button } from "./ui/button";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import { auth } from "../Firebase/firebaseConfig";
+import { signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
+import { Spinner } from "@/components/ui/spinner"
+import { useNavigate } from "react-router-dom";
 const AdminLogin = () => {
+    const navigate = useNavigate()
     const { LoginFormOpen, setLoginFormOpen } = useContext(ModelContext)
     const [mobileNo, setMobileNO] = useState("")
-    const [mobileNoVerification, setMobileNoVerification] = useState(false)
+
     const [password, setPassword] = useState("")
-    const [passwordVerification, setPasswordVerification] = useState(false)
-    const [passwordVerificationMessage, setPasswordVerificationMessage] = useState("")
+
+
     const [adminName, setAdminName] = useState("")
-    const [error, setError] = useState({ adminNameError: "", mobileNoError: "", passwoedError: "" })
+    const [error, setError] = useState({ adminNameError: "", mobileNoError: "", passwordError: "" })
     const [validMobileNo, setValidMobileNo] = useState(false)
+    const [confirmationResult, setconfirmationResult] = useState()
+    const [OTPSend, setOTPSend] = useState(false)
+    const [otp, setotp] = useState('')
+    const [validOTP, setValidOTP] = useState(true)
+    const [formSubmit, setFormSubmit] = useState(false)
+    const setupRecaptcha = () => {
+        try {
+            if (!window.recaptchaVerifier) {
+                window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
+                    size: "invisible",
+                    callback: () => console.log("reCAPTCHA verified ✅"),
+                });
+
+                window.recaptchaVerifier.render().then((widgetId) => {
+                    window.recaptchaWidgetId = widgetId;
+                });
+            } else {
+                // ✅ Reset existing widget if already rendered
+                window.grecaptcha.reset(window.recaptchaWidgetId);
+            }
+        } catch (err) {
+            toast.error(err.message, {
+                position: "top-center",
+                theme: "light",
+                autoClose: 5000
+            })
+            setFormSubmit(false)
+        }
+    };
+
+    const verifyOtp = async () => {
+        try {
+            const result = await confirmationResult.confirm(otp);
+            const user = result.user;
+            const token = await user.getIdToken()
+            const res = await fetch("http://localhost:3000/api/admin-login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            const data = await res.json()
+            const { message } = data
+            console.log(res.ok)
+            if (res.ok) {
+                setOTPSend(false)
+                setotp('')
+                setValidOTP(true)
+
+                toast.success(message, {
+                    toastId: "otpSuccess",
+                    theme: "light",
+                    position: "top-center",
+                    autoClose: 5000
+                })
+                setTimeout(() => {
+                    navigate("/admin")
+                    return
+                }, 5000);
+
+
+            }
+            else if (res.status == 500) {
+                toast.error(message, {
+                    toastId: "otpError500",
+                    theme: "light",
+                    position: "top-center",
+                    autoClose: 5000
+                })
+            } else if (res.status == 404) {
+                toast.error(message, {
+                    toastId: "otpError404",
+                    theme: "light",
+                    position: "top-center",
+                    autoClose: 5000
+                })
+            }
+        } catch (err) {
+            console.log(err);
+            setValidOTP(false)
+        }
+    };
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setFormSubmit(true)
         try {
             const res = await fetch("http://localhost:3000/verify-admin-mobileNo", {
                 method: "POST",
@@ -26,57 +117,94 @@ const AdminLogin = () => {
             })
             const data = await res.json()
             const { success, message } = data
-            setMobileNoVerification(success)
-            setPasswordVerification(success)
-            if (res.ok) {
+
+            if (res.ok && success) {
                 toast.success(message, {
+                    toastId: "mobileSuccess",
                     position: "top-center",
                     theme: "light",
-                    ariaLabel: 5000
+                    autoClose: 5000
                 })
+                const responce = await fetch("http://localhost:3000/verify-password", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify({ password, mobileNo })
+
+                })
+                const responceData = await responce.json()
+                const { verify, inform } = responceData
+                if (verify) {
+                    toast.success(inform, {
+                        toastId: "passwordSuccess",
+                        autoClose: 5000,
+                        theme: "light",
+                        position: "top-center"
+                    })
+                    setupRecaptcha()
+                    const phoneNumber = '+91' + mobileNo;
+                    const appVerifier = window.recaptchaVerifier;
+                    try {
+                        const result = await signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+                        setconfirmationResult(result)
+                        window.confirmationResult = result
+                        setOTPSend(true)
+
+                    } catch (err) {
+                        toast.error(err.message, {
+                            theme: "light",
+                            position: "top-center",
+                            autoClose: 5000
+                        })
+
+                    } finally {
+                        setFormSubmit(false)
+
+                    }
+                } else {
+                    toast.error(inform, {
+                        toastId: "passwordError",
+                        theme: "light",
+                        position: "top-center",
+                        autoClose: 5000
+                    })
+                }
             }
             else {
+                setFormSubmit(false)
                 toast.error(message, {
+                    toastId: "mobileORnameError",
                     position: "top-center",
                     theme: "light",
-                    ariaLabel: 5000
+                    autoClose: 5000
                 })
             }
         } catch (err) {
             console.log(err)
+        } finally {
+            setFormSubmit(false)
         }
-        // try {
 
-        //     const res = await fetch("http://localhost:3000/verify-password", {
-        //         method: "POST",
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             "Accept": "application/json"
-        //         },
-        //         body: JSON.stringify({ password, adminName })
-
-        //     })
-        //     const data = await res.json()
-        //     const { success, message } = data
-        //     setPasswordVerification(success)
-
-        //     setPasswordVerificationMessage(message)
-        //     setError("")
-        //     console.log(success)
-        // } catch (err) {
-        //     console.log(err)
-        // }
-    }
+    };
 
     return (
         <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick
+                pauseOnHover />
+            <div id="recaptcha-container" className="hidden"></div>
             {LoginFormOpen && <div className="fixed top-20 md:top-36 left-1/2  transform -translate-x-1/2 w-4/5 sm:w-3/5 lg:w-2/5 bg-white p-5 rounded-xl shadow-2xl z-40">
                 <div className="flex justify-end ">
                     <button onClick={() => {
                         setLoginFormOpen(!LoginFormOpen)
                         setError("")
-                        setPasswordVerification(false)
-                        setMobileNoVerification(false)
+
                         setMobileNO("")
                         setPassword("")
                         setAdminName("")
@@ -89,7 +217,7 @@ const AdminLogin = () => {
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className="flex flex-col items-center gap-2 text-base font-medium">
-                        {!mobileNoVerification && <Input placeholder="Admin name"
+                        <Input placeholder="Admin name"
                             onChange={e => {
                                 const onlyLetterAndSpace = e.target.value.replace(/[^a-zA-Z\s]/g, "")
                                 setAdminName(onlyLetterAndSpace)
@@ -100,10 +228,10 @@ const AdminLogin = () => {
                                 }
                             }}
                             value={adminName}
-                            className="lg:w-4/6 w-4/5 md:w-3/5" />}
+                            className="lg:w-4/6 w-4/5 md:w-3/5" />
                         {error.adminNameError && <p className="text-red-500"><span><CircleX size={20} className="inline mb-1" /></span>{error.adminNameError}</p>}
 
-                        {!mobileNoVerification && <Input placeholder="Mobile no"
+                        <Input placeholder="Mobile no"
                             type="text"
                             onChange={e => {
                                 const onlyDigits = e.target.value.replace(/\D/g, "")
@@ -121,30 +249,60 @@ const AdminLogin = () => {
                                 }
                             }}
                             value={mobileNo}
-                            className="lg:w-4/6 w-4/5 md:w-3/5" />}
+                            className="lg:w-4/6 w-4/5 md:w-3/5" />
                         {error.mobileNoError && <p className="text-red-500"><span><CircleX size={20} className="inline mb-1" /></span>{error.mobileNoError}</p>}
 
-                        {passwordVerification && <Input placeholder=" Password"
-                            type="text"
+                        <Input placeholder=" Password"
+                            type="password"
                             onChange={e => {
                                 const password = e.target.value;
                                 setPassword(password)
                                 if (!password) {
-                                    setError(prev => ({ ...prev, passwoedError: "Password is required" }))
+                                    setError(prev => ({ ...prev, passwordError: "Password is required" }))
                                 } else {
-                                    setError(prev => ({ ...prev, passwoedError: "" }))
+                                    setError(prev => ({ ...prev, passwordError: "" }))
                                 }
 
                             }}
                             value={password}
-                            className="lg:w-4/6 w-4/5 md:w-3/5" />}
-                        {error.passwoedError && <p className="text-red-500"><span><CircleX size={20} className="inline mb-1" /></span>{error.passwoedError}</p>}
-
-                        {passwordVerification && <button className="py-1 rounded-sm px-3 bg-gradient-to-r text-white hover:scale-105 from-pink-600 to-red-500 transition duration-200">Verify password</button>}
-
-                        {!mobileNoVerification && <Button disabled={!(adminName && mobileNo) || !validMobileNo} className="py-1 rounded-sm px-3 bg-gradient-to-r text-white hover:scale-105 from-pink-600 to-red-500 transition duration-200">Verify mobile no</Button>}
+                            className="lg:w-4/6 w-4/5 md:w-3/5" />
+                        {error.passwordError && <p className="text-red-500"><span><CircleX size={20} className="inline mb-1" /></span>{error.passwordError}</p>}
+                        <Button disabled={!(adminName && mobileNo && password) || !validMobileNo} className="py-1 rounded-sm px-9 bg-gradient-to-r text-white hover:scale-105 from-pink-600 to-red-500 transition duration-200">Verify</Button>
                     </div>
                 </form>
+            </div>}
+            {formSubmit && <div className="fixed inset-0  flex items-center justify-center bg-white/60 z-50">
+                <Spinner className="size-20" color="black" />
+            </div>}
+            {OTPSend && <div className="fixed flex flex-col items-center justify-center gap-3 h-52 w-4/5 md:w-2/5 sm:w-1/2 top-32 left-1/2 transform -translate-x-1/2 bg-white  rounded-xl shadow-2xl z-50">
+                <div>
+                    <h1 className="font-medium  text-green-500">Vefify mobile number</h1>
+                </div>
+                <input
+
+                    maxlength={6}
+                    placeholder="Enter OTP"
+                    type="password"
+                    value={otp}
+                    onChange={e => {
+                        const onlyDigits = e.target.value.replace(/\D/g, "")
+                        setotp(onlyDigits)
+                    }}
+                    className="w-4/5 sm:w-4/5 md:w-3/5  tracking-widest px-4 py-2 border-b-2 border-b-gray-300  focus:outline-none focus:border-b-green-500 text-center shadow-sm text-base font-medium"
+                />
+                {!validOTP &&
+                    <div className="flex flex-row items-center gap-1">
+                        <CircleX color="red" />
+                        <p className="font-medium text-red-500">Enter valid otp</p>
+                    </div>
+                }
+                <button
+                    onClick={verifyOtp}
+                    disabled={otp.length !== 6}
+                    className="bg-gradient-to-r from-green-500 to-emerald-500 text-white font-medium px-3 py-2 rounded-lg hover:scale-x-105 transition duration-200 disabled:opacity-50"
+                >
+                    Verify OTP
+                </button>
             </div>}
 
         </>
