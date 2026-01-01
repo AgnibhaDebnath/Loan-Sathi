bc74b7119f5ef3fdda97b6a8955d7f94f96d0f51
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-01 12:13:51 +0530

    Updated MyloanHeader.jsx

--- src/components/MyLoanHeader.jsx
+++ src/components/MyLoanHeader.jsx
@@ -18,7 +18,7 @@ const MyLoanHeader = () => {
     }
     return (
         <>
-            <header className="fixed w-full top-0 left-0 right-0 z-50 bg-black backdrop-blur-2xl brightness-105 sm:px-8  border-b-2 border-gray-500 ">
+            <header className="fixed w-full top-0 left-0 right-0 z-50 bg-gradient-to-r from-emerald-700 via-green-100 to-green-600 brightness-110 sm:px-8    ">
 
                 <nav className="flex justify-between">
                     <div className='text-3xl font-bold justify-between w-full flex items-center py-2 px-3 gap-2'>
@@ -26,14 +26,14 @@ const MyLoanHeader = () => {
                             <div className='h-12 w-12 rounded-full bg-white flex justify-center items-center'>
                                 <Handshake color='blue' />
                             </div>
-                            <h1 className='font-[Poppins] text-white inline-block'>Loan Sathi</h1>
+                            <h1 className=' text-white font-[poppins]  tracking-wide '>Loan Sathi</h1>
                         </div>
                         <ul className="hidden md:flex">
-                            <div className="flex flex-row items-center gap-16 text-white sm:text-base font-medium text-sm">
-                                <li className="hover:scale-105 transition duration-200"><Link to="/LoanStatusSection">Loan Status</Link></li>
-                                <li className="hover:scale-105 transition duration-200"><Link to="/EMISection">EMI Section</Link></li>
+                            <div className="flex flex-row items-center gap-16 text-white sm:text-base font-semibold text-sm">
+                                <li className="hover:scale-105 transition duration-200 font-[poppins]  hover:text-yellow-300"><a href="/borrower/LoanStatusSection">Loan Status</a></li>
+                                <li className="hover:scale-105 hover:text-yellow-300 transition duration-200 tracking-wide font-semibold font-[poppins]"><a href="/borrower/EMISection">EMI Section</a></li>
                                 <li>
-                                    <button onClick={handleSignout} className="flex flex-row hover:scale-105 transition duration-200"><LogOut />Logout</button>
+                                    <button onClick={handleSignout} className="flex flex-row hover:scale-105 transition duration-200 font-[poppins]"><LogOut className="text-red-300" />Logout</button>
                                 </li>
                             </div>
                         </ul>
@@ -50,11 +50,11 @@ const MyLoanHeader = () => {
             <div className="h-14" aria-hidden="true" />
 
             {isOpen && (
-                <div className="fixed border border-gray-200 right-0 top-16 w-40 flex justify-center p-2 shadow-2xl bg-gray-600 rounded-xl z-50 md:hidden">
-                    <ul className="flex flex-col gap-4 text-white font-medium text-sm px-4 py-2">
-                        <li className="hover:scale-105 transition duration-200"><Link to="/LoanStatusSection">Loan status</Link></li>
-                        <li className="hover:scale-105 transition duration-200"><Link to="/EMISection">EMI section</Link></li>
-                        <li className="flex flex-row items-center gap-1">
+                <div className="fixed bg-white  right-0 top-16 w-40 flex justify-center p-2 shadow-2xl  rounded-xl z-50 md:hidden">
+                    <ul className="flex flex-col gap-4 text-black font-semibold text-sm px-4 py-2 font-[poppins]">
+                        <li className="hover:scale-105 transition duration-200 "><a href="/borrower/LoanStatusSection ">Loan status</a></li>
+                        <li className="hover:scale-105 transition duration-200 font-[poppins] "><a className="" href="/borrower/EMISection">EMI section</a></li>
+                        <li className="flex flex-row items-center gap-1 font-[poppins]">
                             <LogOut color="red" />
                             <button onClick={handleSignout}>Logout</button>
                         </li>
