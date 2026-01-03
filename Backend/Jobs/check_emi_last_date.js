2daf2037bbb4eb316a85cc89271de576bfacd138
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-04 02:37:55 +0530

    Adding check_emi_last_date.js

---
+++ Backend/Jobs/check_emi_last_date.js
@@ -0,0 +1,18 @@
+const connection=require("../DB/db")
+
+const check_last_emi_due_date = async () => {
+    try {
+        
+        const [rows] = await connection.execute("SELECT loan_id,MAX(installment_no) as last_installment_no,MAX(due_date) as last_due_date  FROM emi_schedule_for_borrower GROUP BY loan_id HAVING MAX(due_date)=?",[new Date().toLocaleDateString("en-CA")])
+        if (rows.length > 0){
+      return rows  
+        } else {
+            return []
+}
+   
+    } catch (err) {
+        console.log(err)
+          }
+
+}
+module.exports={check_last_emi_due_date}
