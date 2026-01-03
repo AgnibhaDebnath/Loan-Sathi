627c708c84920d09c1bfd438b49ad7a0eaaec7c2
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-04 02:36:54 +0530

    Adding update_total_outstanding.j

---
+++ Backend/Jobs/update_total_outstanding.js
@@ -0,0 +1,22 @@
+const connection=require("../DB/db")
+const update_total_outstanding = async (loanID, interestComponent, emi_amount) => {
+    console.log(loanID, interestComponent, emi_amount)
+    try {
+        const [emiDetails] = await connection.execute("SELECT outstanding_added FROM emi_schedule_for_borrower WHERE loan_id=?", [loanID])
+        if (!emiDetails.outstanding_added) {
+            const [rows] = await connection.execute("UPDATE loan_details SET total_outstanding=total_outstanding + ? WHERE loan_id=?", [emi_amount + interestComponent, loanID]) 
+            await connection.execute("UPDATE emi_schedule_for_borrower SET outstanding_added=? WHERE loan_id=?",["true",loanID])
+       
+        if (rows.affectedRows == 0) {
+            return false
+            } 
+            return true
+        } else {
+            return true
+             }
+    } catch (err) {
+        console.log(err)
+        return false
+      }
+}
+module.exports={update_total_outstanding}
