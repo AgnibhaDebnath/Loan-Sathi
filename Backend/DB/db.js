ac62654eac49c67bde0ab37640dc8670496ef4ec
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-09 19:42:17 +0530

    Updatd db.js

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -2,8 +2,8 @@
 
     // Create a connection object
     const connection = mysql.createPool({
-        host: 'localhost', // Or your MySQL server host
-        user: 'root', // Your MySQL username
+        host: process.env.MYSQLHOST, // Or your MySQL server host
+        user: process.env.DB_USER, // Your MySQL username
         password: process.env.DATABASE_PASSWORD, // Your MySQL password
         database: process.env.DATABASE, // The database you want to connect to
         port:process.env.DB_PORT
