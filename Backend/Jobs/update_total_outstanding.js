b1c506b987e94aa8526a0b3adc8e60ce3e0e91be
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-10 13:28:44 +0530

    Update update_total_outstanding file

--- Backend/Jobs/update_total_outstanding.js
+++ Backend/Jobs/update_total_outstanding.js
@@ -2,8 +2,9 @@ const connection=require("../DB/db")
 const update_total_outstanding = async (loanID, emi_amount) => {
     console.log(loanID,  emi_amount)
     try {
-        const [emiDetails] = await connection.execute("SELECT outstanding_added FROM emi_schedule_for_borrower WHERE loan_id=?", [loanID])
-        if (!emiDetails.outstanding_added) {
+        const [emiDetails] = await connection.execute("SELECT outstanding_added FROM emi_schedule_for_borrower WHERE loan_id=? ", [loanID])
+        
+        if (!emiDetails[0].outstanding_added) {
             const [rows] = await connection.execute("UPDATE loan_details SET total_outstanding=total_outstanding + ? WHERE loan_id=?", [emi_amount , loanID]) 
             await connection.execute("UPDATE emi_schedule_for_borrower SET outstanding_added=? WHERE loan_id=?",["true",loanID])
        
