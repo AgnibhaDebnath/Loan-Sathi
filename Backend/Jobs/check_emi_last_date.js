e3f822d8bbba343202f3e9662a462e9201c33a39
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-01-10 22:13:28 +0530

    Updated cron-job files

--- Backend/Jobs/check_emi_last_date.js
+++ Backend/Jobs/check_emi_last_date.js
@@ -3,7 +3,7 @@ const connection=require("../DB/db")
 const check_last_emi_due_date = async () => {
     try {
         
-        const [rows] = await connection.execute("SELECT loan_id,MAX(installment_no) as last_installment_no,MAX(due_date) as last_due_date  FROM emi_schedule_for_borrower GROUP BY loan_id HAVING MAX(due_date)=?",[new Date().toLocaleDateString("en-CA")])
+        const [rows] = await connection.execute("SELECT loan_id,MAX(installment_no) as last_installment_no,MAX(due_date) as last_due_date  FROM emi_schedule_for_borrower WHERE amount_paid >=(emi_amount + penalty) GROUP BY loan_id HAVING MAX(due_date)=?",[new Date().toLocaleDateString("en-CA")])
         if (rows.length > 0){
       return rows  
         } else {
--- Backend/Jobs/getEMIsWithDueDateToday.js
+++ Backend/Jobs/getEMIsWithDueDateToday.js
@@ -2,7 +2,7 @@ const connection=require("../DB/db")
 
 const getEMIsWithDueDateToday = async () => {
     try {
-            const [rows] = await connection.execute("SELECT loan_id,MAX(due_date) as due_date FROM emi_schedule_for_borrower  GROUP BY loan_id HAVING MAX(due_date)=?", [new Date().toLocaleDateString("en-CA")])
+            const [rows] = await connection.execute("SELECT loan_id,MAX(due_date) as due_date, FROM emi_schedule_for_borrower  GROUP BY loan_id HAVING MAX(due_date)=?", [new Date().toLocaleDateString("en-CA")])
         if (rows.length > 0) {
                 return rows  
         } else {
