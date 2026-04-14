380b9ad6ac08fbf27c184ec8a370c2f9845f10d1
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-14 17:47:28 +0530

    feat(auth): implement signup controller with validation and password hashing

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -1,5 +1,20 @@
+const user = require("../Model/UserModel")
+const bcrypt=require('bcrypt')
+exports.SignUp = async(req, res, next) => {
+    try {
+   const { name, email, password } = req.body;  
+   const hashedPassword = await bcrypt.hash(password, 12);
+   await user.create({
+       name,
+       email,
+       password:hashedPassword,
+   })
+    res.status(201).json({success:true,message:"Account created successfully 🎉"})
+    } catch (err) {
+    res.status(500).json({success:false,message:"Something went wrong. Please try again later"});
+    }
+}
 
-exports.signUp = (req,res,next) => {
-    console.log(req.body)
-    res.send("From submitted")
+exports.Login = (req,res,next) => {
+    
 }
