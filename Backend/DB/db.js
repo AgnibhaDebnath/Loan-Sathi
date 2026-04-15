e55230e4e698a48ed6f2b4444127aeb6a30d7dce
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-15 08:12:17 +0530

    refactor(db): migrate database connection from MySQL to MongoDB

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -1,11 +1,12 @@
-   const mysql=require("mysql2/promise")
+const mongoose = require("mongoose")
 
-    // Create a connection object
-    const connection = mysql.createPool({
-        host: process.env.MYSQLHOST, // Or your MySQL server host
-        user: process.env.DB_USER, // Your MySQL username
-        password: process.env.DATABASE_PASSWORD, // Your MySQL password
-        database: process.env.DATABASE, // The database you want to connect to
-        port:process.env.DB_PORT
-    });
-module.exports = connection;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Successfully connected to MongoDB")
+    } catch (err) {
+        console.log("Error while connecting to DB")
+   }
+}
+
+connectDB()
